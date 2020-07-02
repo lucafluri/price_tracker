af629502ea4197ac7da573919c89692a4b7066e2
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-07-02 12:59:22 +0200

    merged changes from dev-branch

--- price_tracker/lib/main.dart
+++ price_tracker/lib/main.dart
@@ -1,6 +1,6 @@
+import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:price_tracker/utils/database_helper.dart';
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
@@ -23,7 +23,7 @@ String appName = "Price Tracker BETA";
 // Things todo:
 //-------
 // TODO Rewrite Parser to Strategy Pattern => parse xpaths from json on app start from github
-  // Solves several problems, faster iteration process, store adding and parser quickfixes
+// Solves several problems, faster iteration process, store adding and parser quickfixes
 // TODO Disable Adding of Elements if no Internet Connection available (recheck every initState of main)
 // TODO Testing: Tests and Test Button in app, that tests Notifications, background Service and update functionality
 // TODO Better Icon
@@ -35,12 +35,10 @@ String appName = "Price Tracker BETA";
 // TODO Settings/Credits Screen
 // TODO Intro Screen sizing on all types (rewrite/change intro screen to interactive tutorial or simple help page)
 
-
 // BETA BUG LIST
 //--------------
 // TODO Notification only once per day (or when price changes again)
 // TODO Touch and drag of graph is outside graph on the left
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -49,7 +47,6 @@ void main() async {
 
   runApp(MyApp());
 }
-
 
 class MyBehavior extends ScrollBehavior {
   @override
