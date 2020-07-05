bd85dbc0fb608478de0fd3d43f28a99c801e8735
Luca Fluri <luca.fluri4@gmail.com>
2020-07-06 00:28:26 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
   //TODO Replace with real tests
   test("Test test", () {
