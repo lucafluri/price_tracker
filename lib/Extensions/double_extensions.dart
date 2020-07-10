4bb075025cf209118ec470a39779032a722a0cf8
Luca Fluri <luca.fluri4@gmail.com>
2020-07-10 21:10:00 +0200

    removed extensions folder

--- lib/Extensions/double_extensions.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:math';
-
-extension DoubleExtensions on double {
-  double roundToPlace(int places) {
-    double mod = pow(10.0, places);
-    return ((this * mod).round().toDouble() / mod);
-  }
-}
