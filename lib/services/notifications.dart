47ff36e7f53dedc0e212dffeaa6627e8394c419d
Luca Fluri <luca.fluri4@gmail.com>
2020-07-16 03:56:38 +0200

    fix: show correct notification icon on android
    - drawable resource no longer discarded when building in release config

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -7,7 +7,9 @@ import 'package:rxdart/subjects.dart';
 
 class NotificationService {
   static FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin;
+  static NotificationAppLaunchDetails notificationAppLaunchDetails;
   static String currentPayload;
+
   NotificationService._privateConstructor();
 
   static final NotificationService _instance =
@@ -17,6 +19,9 @@ class NotificationService {
 
   static Future<void> init() async {
     _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    // Info if app was launched from notification
+    notificationAppLaunchDetails = await _flutterLocalNotificationsPlugin
+        .getNotificationAppLaunchDetails();
 
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     var androidInitializationSettings =
@@ -52,8 +57,14 @@ class NotificationService {
       int id, String title, String body,
       {String payload = ""}) async {
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        '0', 'Price Alerts', 'Price change alerts for tracked products',
-        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+      '0',
+      'Price Alerts',
+      'Price change alerts for tracked products',
+      importance: Importance.Max,
+      priority: Priority.High,
+      // ticker: 'ticker',
+      // icon: 'app_icon',
+    );
     var iOSPlatformChannelSpecifics = IOSNotificationDetails();
     var platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
@@ -71,7 +82,8 @@ class NotificationService {
         '0', 'Price Alerts', 'Price change alerts for tracked products',
         importance: Importance.Max,
         priority: Priority.High,
-        ticker: 'ticker',
+        // ticker: 'ticker',
+        // icon: 'app_icon',
         styleInformation: BigTextStyleInformation(body));
     var iOSPlatformChannelSpecifics = IOSNotificationDetails();
     var platformChannelSpecifics = NotificationDetails(
